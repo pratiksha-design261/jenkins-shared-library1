cb67dc991f7fc3bebf06d0f5c9a0268c1fbe12e4
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-05-28 00:23:31 +0530

    Update dockerBuild.groovy
    Updated for credID change

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,7 +3,7 @@ def call(String project, String hubUser) {
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
-            credentialsId: "docker_cred",
+            credentialsId: "docker_User",
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
